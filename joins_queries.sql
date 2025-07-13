68ce53e1240dacf75d5d730cd34571d3d4e45f9f
adika-oloo <66367717+adika-oloo@users.noreply.github.com>
2025-07-13 12:10:16 +0300

    Update joins_queries.sql

--- joins_queries.sql
+++ joins_queries.sql
@@ -1,3 +1,9 @@
 SELECT b.*, u.name, u.email
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
+SELECT p.*, r.rating, r.comment
+FROM properties p
+LEFT JOIN reviews r ON p.id = r.property_id;
+SELECT u.*, b.*
+FROM users u
+FULL OUTER JOIN bookings b ON u.id = b.user_id;
