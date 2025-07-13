4ad7c6e4af9f078daeba1bedfb91f4a5dc499dd9
adika-oloo <66367717+adika-oloo@users.noreply.github.com>
2025-07-13 12:14:15 +0300

    Create subqueries.sql

---
+++ subqueries.sql
@@ -0,0 +1,17 @@
+##Non-correlated subquery:
+SELECT * FROM properties
+WHERE id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+##Correlated subquery:
+
+SELECT * FROM users u
+WHERE (
+    SELECT COUNT(*) 
+    FROM bookings b
+    WHERE b.user_id = u.id
+) > 3;
+
